c9d3dbc82dfc902de087da5bca78aeed3b1c5160
sponser <sponser@life.localdomain>
2023-06-30 09:51:11 +0300

    modifying urls

--- savings/urls.py
+++ savings/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("core.urls")),
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r"^.*(?!(media))", TemplateView.as_view(template_name="index.html"))]
